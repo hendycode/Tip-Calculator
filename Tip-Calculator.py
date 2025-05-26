a2b76aa098f279451100dcb871c51ce6dff196ae
hendycode <ratemoneon@gmail.com>
2025-05-26 14:58:13 +0300

    evening

---
+++ Tip-Calculator.py
@@ -0,0 +1,9 @@
+print("Welcome to the the tip calculator!")
+input("What was the total bill? ")
+input("How much tip would you like to give? (%) ")
+input("How many people will split the bill? ")
+
+perbill 
+pertip = tiptotal / nopeeps
+total = perbill + pertip
+print(f"Alright, so each person will pay {total}, thank you for visting us")
