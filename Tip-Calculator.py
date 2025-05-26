94ebeeef0a05bef794777fe26cf84d9ab1e75836
hendycode <ratemoneon@gmail.com>
2025-05-26 15:05:44 +0300

    easypeasy

--- Tip-Calculator.py
+++ Tip-Calculator.py
@@ -1,9 +1,9 @@
 print("Welcome to the the tip calculator!")
-input("What was the total bill? ")
-input("How much tip would you like to give? (%) ")
-input("How many people will split the bill? ")
+totalbill = int(input("What was the total bill? "))
+tiptotal = totalbill * (int(input("How much tip would you like to give? (%) ")) / 100)
+nopeeps = int(input("How many people will split the bill? "))
 
-perbill 
+perbill = totalbill / nopeeps
 pertip = tiptotal / nopeeps
 total = perbill + pertip
-print(f"Alright, so each person will pay {total}, thank you for visting us")
+print(f"Alright, so each person will pay Ksh{total}, thank you for visting us")
